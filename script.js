cadabec67eecafb7f7cb742201ccf15dfb15eae7
ZayanIbneZia <ninjagamerz900@gmail.com>
2024-12-24 08:30:12 -0800

    Creating a portfolio website

---
+++ script.js
@@ -0,0 +1,7 @@
+function getOwlInfo() {
+    document.getElementById('owlinfo').innerHTML = "Owls are nocturnal birds of prey belonging to the orer Strigiformes";
+}
+
+function getPandaInfo() {
+    document.getElementById("pandainfo").innerHTML = "Pandas are large, herbivorous mammals native to China, known for ther distinct black and wite fur.";
+}
